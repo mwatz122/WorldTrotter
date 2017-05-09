b571d090cd1f85ec8eb67168c29910662bd2ba03
Matthew A. Watzman <mwatz122@gmail.com>
2017-05-09 08:57:58 -0400

    added MapViewController

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Matthew Watzman on 5/9/17.
+//  Copyright © 2017 Matthew Watzman. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController : UIViewController
+{
+
+}
